82d8b3a305d9d3eb7ecf6a3bd3499b6f01676b74
Eugene Fox <eugene@xfox111.net>
2024-12-25 13:57:38 +0300

    Added state save for advanced generator mode

--- entrypoints/advanced/Page.tsx
+++ entrypoints/advanced/Page.tsx
@@ -9,11 +9,22 @@ import PasswordSection from "./sections/PasswordSection";
 
 export default function Page(): ReactElement
 {
-	const [isPassphrase, setIsPassphrase] = useState<boolean>(false);
+	const [isPassphrase, setIsPassphrase] = useState<boolean | null>(null);
 	const [passwords, setPasswords] = useState<string[]>([]);
 	const isSmall = useMediaQuery({ query: "(max-width: 1000px)" });
 
 	const cls = useStyles();
+
+	useEffect(() =>
+	{
+		advancedPassphraseSelected.getValue().then(setIsPassphrase);
+		const unwatch = advancedPassphraseSelected.watch(setIsPassphrase);
+
+		return () => unwatch();
+	}, []);
+
+	if (isPassphrase === null)
+		return <></>;
 
 	return (
 		<main className={ mergeClasses(cls.root, isSmall && cls.smallRoot) }>
@@ -24,7 +35,7 @@ export default function Page(): ReactElement
 			<article className={ mergeClasses(cls.configRoot, isSmall && cls.hideScroll) }>
 				<DoubleLabledSwitch outerRoot={ { className: cls.switch } }
 					checked={ isPassphrase }
-					onChange={ (_, e) => setIsPassphrase(e.checked) }
+					onChange={ (_, e) => advancedPassphraseSelected.setValue(e.checked) }
 					offLabel={ i18n.t("advanced.password.title") }
 					onLabel={ i18n.t("advanced.passphrase.title") } />
 
@@ -43,3 +54,5 @@ export type GeneratorProps =
 	{
 		onGenerated: (passwords: string[]) => void;
 	};
+
+const advancedPassphraseSelected = storage.defineItem<boolean>("sync:AdvancedPassphraseSelected", { fallback: false });
