cd29ec5ab2469603516f0858009ee3eb41c8102f
Eugene Fox <eugene@xfox111.net>
2025-06-30 11:52:49 +0300

    Force no-wrap for special characters tooltip (resolves #551)

--- utils/infoLabel.tsx
+++ utils/infoLabel.tsx
@@ -1,15 +1,24 @@
-import { InfoLabel, Label, LabelProps, Slot } from "@fluentui/react-components";
+import { InfoLabel, Label, LabelProps, makeStyles, Slot } from "@fluentui/react-components";
 
 // FIXME: Remove ts-ignore comments once slots override fix is released
 // Tracker: https://github.com/microsoft/fluentui/issues/27090
 
-export default function infoLabel(label: string, hint: string): Slot<typeof Label>
+export default function infoLabel(label: string, hint: string, noWrap?: boolean): Slot<typeof Label>
 {
+	const cls = useStyles();
+
 	// @ts-expect-error See FIXME
 	return {
 		children: (_: unknown, props: LabelProps) =>
-			<InfoLabel { ...props } info={ hint }>
+			<InfoLabel { ...props } info={ { className: noWrap ? cls.noWrap : undefined, children: hint } }>
 				{ label }
 			</InfoLabel>
 	};
 }
+
+const useStyles = makeStyles({
+	noWrap:
+	{
+		whiteSpace: "pre"
+	}
+});
