e1a4e0913267684d9b57350ed81d80d9bf178ad0
Eugene Fox <eugene@xfox111.net>
2025-07-01 11:23:46 +0300

    Removed ts-expect-error microsoft/fluentui#27090

--- utils/infoLabel.tsx
+++ utils/infoLabel.tsx
@@ -1,13 +1,9 @@
 import { InfoLabel, Label, LabelProps, makeStyles, Slot } from "@fluentui/react-components";
 
-// FIXME: Remove ts-ignore comments once slots override fix is released
-// Tracker: https://github.com/microsoft/fluentui/issues/27090
-
 export default function infoLabel(label: string, hint: string, noWrap?: boolean): Slot<typeof Label>
 {
 	const cls = useStyles();
 
-	// @ts-expect-error See FIXME
 	return {
 		children: (_: unknown, props: LabelProps) =>
 			<InfoLabel { ...props } info={ { className: noWrap ? cls.noWrap : undefined, children: hint } }>
